3327425ab6f5481926d20be68bd358c74dc68ee3
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-06 17:43:22 -0300

    Demo: Hosting in Console App

---
+++ src/Zza.SelfHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+using ZzaApp.Services;
+
+namespace Zza.SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var host = new ServiceHost(typeof(ZzaService));
+                host.Open();
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
